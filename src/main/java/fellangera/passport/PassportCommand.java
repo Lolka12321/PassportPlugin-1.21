3bbf163f6f35aee12b82222bec1a044995543df1
Fellangera <86649162+Lolka12321@users.noreply.github.com>
2026-01-21 11:12:25 +0300

    Refactor PassportCommand for improved readability

--- src/main/java/fellangera/passport/PassportCommand.java
+++ src/main/java/fellangera/passport/PassportCommand.java
@@ -3,7 +3,9 @@ package fellangera.passport;
 import fellangera.passport.util.ColorUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.command.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
@@ -13,74 +15,138 @@ public class PassportCommand implements CommandExecutor {
     private final PassportManager manager;
     private final PassportPlugin plugin;
 
-    public PassportCommand(PassportManager m, PassportPlugin p) {
-        manager = m;
-        plugin = p;
+    public PassportCommand(PassportManager manager, PassportPlugin plugin) {
+        this.manager = manager;
+        this.plugin = plugin;
     }
 
     @Override
-    public boolean onCommand(CommandSender s, Command c, String l, String[] a) {
-        if (!(s instanceof Player p)) return true;
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if (a.length == 1 && a[0].equalsIgnoreCase("help")) {
-            p.sendMessage(ColorUtil.toComponent(plugin.lang().getString("help.title")));
-            p.sendMessage(ColorUtil.toComponent(plugin.lang().getString("help.passport")));
-            if (p.hasPermission("passport.check"))
-                p.sendMessage(ColorUtil.toComponent(plugin.lang().getString("help.check")));
-            if (p.hasPermission("passport.reload"))
-                p.sendMessage(ColorUtil.toComponent(plugin.lang().getString("help.reload")));
+        if (!(sender instanceof Player player)) {
             return true;
         }
 
-        if (a.length == 1 && a[0].equalsIgnoreCase("reload")) {
-            if (!p.hasPermission("passport.reload")) {
-                p.sendMessage(ColorUtil.toComponent(plugin.lang().getString("errors.no-permission")));
+        // /passport help
+        if (args.length == 1 && args[0].equalsIgnoreCase("help")) {
+
+            player.sendMessage(
+                    ColorUtil.toComponent(plugin.lang().getString("help.title"))
+            );
+
+            player.sendMessage(
+                    ColorUtil.toComponent(plugin.lang().getString("help.passport"))
+            );
+
+            if (player.hasPermission("passport.check")) {
+                player.sendMessage(
+                        ColorUtil.toComponent(plugin.lang().getString("help.check"))
+                );
+            }
+
+            if (player.hasPermission("passport.reload")) {
+                player.sendMessage(
+                        ColorUtil.toComponent(plugin.lang().getString("help.reload"))
+                );
+            }
+
+            return true;
+        }
+
+        // /passport reload
+        if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
+
+            if (!player.hasPermission("passport.reload")) {
+                player.sendMessage(
+                        ColorUtil.toComponent(plugin.lang().getString("errors.no-permission"))
+                );
                 return true;
             }
+
             plugin.reloadAll();
-            p.sendMessage(ColorUtil.toComponent(plugin.lang().getString("success.reloaded")));
+
+            player.sendMessage(
+                    ColorUtil.toComponent(plugin.lang().getString("success.reloaded"))
+            );
+
             return true;
         }
 
-        if (a.length == 2 && a[0].equalsIgnoreCase("check")) {
-            if (!p.hasPermission("passport.check")) {
-                p.sendMessage(ColorUtil.toComponent(plugin.lang().getString("errors.no-permission")));
+        // /passport check <player>
+        if (args.length == 2 && args[0].equalsIgnoreCase("check")) {
+
+            if (!player.hasPermission("passport.check")) {
+                player.sendMessage(
+                        ColorUtil.toComponent(plugin.lang().getString("errors.no-permission"))
+                );
                 return true;
             }
-            Player t = Bukkit.getPlayerExact(a[1]);
-            if (t == null || manager.getPassport(t) == null) {
-                p.sendMessage(ColorUtil.toComponent(plugin.lang().getString("errors.no-passport")));
+
+            Player target = Bukkit.getPlayerExact(args[1]);
+            if (target == null) {
+                player.sendMessage(
+                        ColorUtil.toComponent(plugin.lang().getString("errors.player-not-found"))
+                );
                 return true;
             }
-            open(p, manager.getPassport(t));
+
+            Passport passport = manager.getPassport(target);
+            if (passport == null) {
+                player.sendMessage(
+                        ColorUtil.toComponent(plugin.lang().getString("errors.no-passport"))
+                );
+                return true;
+            }
+
+            openPassportBook(player, passport);
             return true;
         }
 
-        Passport ps = manager.getPassport(p);
-        if (ps == null) {
-            p.sendMessage(ColorUtil.toComponent(plugin.lang().getString("errors.no-passport")));
+        // /passport
+        if (args.length == 0) {
+
+            Passport passport = manager.getPassport(player);
+            if (passport == null) {
+                player.sendMessage(
+                        ColorUtil.toComponent(plugin.lang().getString("errors.no-passport"))
+                );
+                return true;
+            }
+
+            openPassportBook(player, passport);
             return true;
         }
-        open(p, ps);
+
         return true;
     }
 
-    private void open(Player p, Passport ps) {
-        ItemStack b = new ItemStack(Material.WRITTEN_BOOK);
-        BookMeta m = (BookMeta) b.getItemMeta();
-        m.setTitle(plugin.getConfig().getString("passport.book.title").replace("&",""));
-        m.setAuthor(plugin.getConfig().getString("passport.book.author").replace("&",""));
-        for (String r : plugin.getConfig().getStringList("passport.book.pages")) {
-            m.addPages(ColorUtil.toComponent(
-                    r.replace("%name%", ps.getName())
-                            .replace("%surname%", ps.getSurname())
-                            .replace("%age%", String.valueOf(ps.getAge()))
-                            .replace("%region%", ps.getRegion())
-                            .replace("%series%", ps.getSeries())
-                            .replace("%number%", String.valueOf(ps.getNumber()))
-            ));
+    private void openPassportBook(Player viewer, Passport passport) {
+
+        ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
+        BookMeta meta = (BookMeta) book.getItemMeta();
+
+        String title = plugin.lang().getString("book.title", "Passport");
+        String author = plugin.lang().getString("book.author", "Government");
+
+        meta.setTitle(title.replace("&", ""));
+        meta.setAuthor(author.replace("&", ""));
+
+        for (String rawPage : plugin.lang().getStringList("book.pages")) {
+
+            String page = rawPage
+                    .replace("%name%", passport.getName())
+                    .replace("%surname%", passport.getSurname())
+                    .replace("%age%", String.valueOf(passport.getAge()))
+                    .replace("%region%", passport.getRegion())
+                    .replace("%series%", passport.getSeries())
+                    .replace("%number%", String.valueOf(passport.getNumber()))
+                    .replace("%passport_id%",
+                            passport.getSeries() + " " + passport.getNumber());
+
+            meta.addPages(ColorUtil.toComponent(page));
         }
-        b.setItemMeta(m);
-        p.openBook(b);
+
+        book.setItemMeta(meta);
+        viewer.openBook(book);
     }
 }
