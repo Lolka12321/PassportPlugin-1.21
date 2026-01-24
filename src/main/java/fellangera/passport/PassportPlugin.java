27c2a966cbd11aff20ee3a2f59ae8ec02063bbe7
Fellangera <86649162+Lolka12321@users.noreply.github.com>
2026-01-24 12:09:30 +0300

    Update PassportPlugin.java

--- src/main/java/fellangera/passport/PassportPlugin.java
+++ src/main/java/fellangera/passport/PassportPlugin.java
@@ -7,34 +7,59 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 
 public class PassportPlugin extends JavaPlugin {
-
     private static PassportPlugin instance;
     private PassportManager passportManager;
+    private PassportRequest requestManager;
     private FileConfiguration lang;
 
     @Override
     public void onEnable() {
         instance = this;
+
+        // Загружаем конфигурацию
         saveDefaultConfig();
         loadLanguage();
+
+        // Инициализация менеджеров
         passportManager = new PassportManager(this);
-        getCommand("passport").setExecutor(new PassportCommand(passportManager, this));
+        requestManager = new PassportRequest(this, passportManager);
+
+        // Регистрация команд
+        PassportCommand commandExecutor = new PassportCommand(passportManager, this, requestManager);
+        requestManager.setCommandHandler(commandExecutor);
+
+        getCommand("passport").setExecutor(commandExecutor);
         getCommand("passport").setTabCompleter(new PassportTabCompleter());
+
+        // Регистрация событий
         getServer().getPluginManager().registerEvents(
-                new PassportListener(passportManager, this), this
+                new PassportListener(passportManager, this),
+                this
         );
+
+        getLogger().info("PassportPlugin успешно загружен!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("PassportPlugin выгружен!");
     }
 
     public void reloadAll() {
         reloadConfig();
         loadLanguage();
+        getLogger().info("Конфигурация и локализация перезагружены!");
     }
 
     private void loadLanguage() {
-        String name = getConfig().getString("language", "en-EN");
-        File file = new File(getDataFolder(), name + ".yml");
-        if (!file.exists()) saveResource(name + ".yml", false);
-        lang = YamlConfiguration.loadConfiguration(file);
+        String languageName = getConfig().getString("language", "en-EN");
+        File languageFile = new File(getDataFolder(), languageName + ".yml");
+
+        if (!languageFile.exists()) {
+            saveResource(languageName + ".yml", false);
+        }
+
+        lang = YamlConfiguration.loadConfiguration(languageFile);
     }
 
     public FileConfiguration lang() {
