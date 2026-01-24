29bf63d63c71fe8d59019d015b7e17a656ad38e7
Fellangera <86649162+Lolka12321@users.noreply.github.com>
2026-01-25 02:55:10 +0300

    Integrate PassportGUI and enhance language loading
    Added PassportGUI integration and improved language file handling.

--- src/main/java/fellangera/passport/PassportPlugin.java
+++ src/main/java/fellangera/passport/PassportPlugin.java
@@ -5,61 +5,96 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 public class PassportPlugin extends JavaPlugin {
     private static PassportPlugin instance;
     private PassportManager passportManager;
     private PassportRequest requestManager;
+    private PassportGUI gui;
     private FileConfiguration lang;
 
     @Override
     public void onEnable() {
         instance = this;
 
-        // Загружаем конфигурацию
         saveDefaultConfig();
         loadLanguage();
 
-        // Инициализация менеджеров
         passportManager = new PassportManager(this);
         requestManager = new PassportRequest(this, passportManager);
+        gui = new PassportGUI(this, passportManager);
 
-        // Регистрация команд
-        PassportCommand commandExecutor = new PassportCommand(passportManager, this, requestManager);
+        PassportCommand commandExecutor = new PassportCommand(
+                passportManager, this, requestManager, gui);
         requestManager.setCommandHandler(commandExecutor);
 
-        getCommand("passport").setExecutor(commandExecutor);
-        getCommand("passport").setTabCompleter(new PassportTabCompleter());
+        if (getCommand("passport") != null) {
+            getCommand("passport").setExecutor(commandExecutor);
+            getCommand("passport").setTabCompleter(new PassportTabCompleter());
+        } else {
+            getLogger().severe("Failed to register /passport command!");
+        }
 
-        // Регистрация событий
         getServer().getPluginManager().registerEvents(
                 new PassportListener(passportManager, this),
                 this
         );
+        getServer().getPluginManager().registerEvents(gui, this);
 
-        getLogger().info("PassportPlugin успешно загружен!");
+        getLogger().info("PassportPlugin v1.0 successfully loaded!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("PassportPlugin выгружен!");
+        getLogger().info("PassportPlugin unloaded!");
     }
 
     public void reloadAll() {
         reloadConfig();
         loadLanguage();
-        getLogger().info("Конфигурация и локализация перезагружены!");
+        gui.reloadGuiConfig();
+        getLogger().info("Configuration and localization reloaded!");
     }
 
     private void loadLanguage() {
         String languageName = getConfig().getString("language", "en-EN");
-        File languageFile = new File(getDataFolder(), languageName + ".yml");
+
+        File langFolder = new File(getDataFolder(), "lang");
+        if (!langFolder.exists()) {
+            langFolder.mkdirs();
+        }
+
+        File languageFile = new File(langFolder, languageName + ".yml");
 
         if (!languageFile.exists()) {
-            saveResource(languageName + ".yml", false);
+            try {
+                InputStream resourceStream = getResource(languageName + ".yml");
+                if (resourceStream != null) {
+                    Files.copy(resourceStream, languageFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                    resourceStream.close();
+                    getLogger().info("Created language file: " + languageName + ".yml");
+                } else {
+                    getLogger().warning("Language file " + languageName + ".yml not found in resources!");
+                    if (!languageName.equals("en-EN")) {
+                        languageName = "en-EN";
+                        languageFile = new File(langFolder, "en-EN.yml");
+                        resourceStream = getResource("en-EN.yml");
+                        if (resourceStream != null) {
+                            Files.copy(resourceStream, languageFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                            resourceStream.close();
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                getLogger().severe("Failed to create language file: " + e.getMessage());
+            }
         }
 
         lang = YamlConfiguration.loadConfiguration(languageFile);
+        getLogger().info("Loaded language: " + languageName);
     }
 
     public FileConfiguration lang() {
